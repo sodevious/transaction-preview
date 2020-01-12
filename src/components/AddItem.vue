1824a3979b6d4c7dafefe9a87346b11ba562c13e
Nicole Dominguez <sodevious.net@gmail.com>
2020-01-11 20:38:06 -0800

    basic localStorage prototype

---
+++ src/components/AddItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <form>
+    <input type="text" v-model="name" placeholder="add an item..." />
+    <input type="text" v-model="price" placeholder="$" />
+    <button @click="persist" type="submit">Save</button>
+
+    <br />
+    <br />
+
+    <p>
+      {{ name }}
+    </p>
+
+    <p>
+      {{ price }}
+    </p>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'add-item',
+    data: function () {
+      return {
+        name: '',
+        price: 0
+      }
+    },
+    mounted() {
+      if (localStorage.name) {
+        this.name = localStorage.name;
+      }
+      if (localStorage.price) {
+        this.price = localStorage.price;
+      }
+    },
+    methods: {
+      persist() {
+        localStorage.name = this.name;
+        localStorage.price = this.price;
+      }
+    }
+  }
+</script>
